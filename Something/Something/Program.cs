5f4663f00a7dad15058f00c5fa75b964ace11134
QQais <Qais.Qaisi@revature.net>
2021-09-08 16:50:42 -0500

    challange code

---
+++ Something/Something/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Something
+{
+    class Program
+    {
+        public static string GiveMeSomething(string word)
+        {
+            return "Somthing "+ word;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GiveMeSomething("is better than nothing"));
+        }
+    }
+}
